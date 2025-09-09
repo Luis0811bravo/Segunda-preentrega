cfcd4651d892e1acab21f972b09c95af024c3054
Adrian Bravo <adrian081191@gmail.com>
2025-09-08 21:08:38 -0600

    Correcciones preentrega 2

--- script/script.js
+++ script/script.js
@@ -2,12 +2,27 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaProductos = document.querySelector('#lista-productos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
     // Cuando agregas un curso presionando "Agregar al Carrito"
     listaProductos.addEventListener('click', agregarCurso);
 
+    // Elimina productos del carrito
+    carrito.addEventListener('click', eliminarCurso);
+
+    // Cargar productos del Local Storage
+    document.addEventListener('DOMContentLoaded', () => {
+        articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        carritoHTML();
+    });
+
+    // Vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', () => {
+        articulosCarrito = []; // Reseteamos el arreglo
+        limpiarHTML(); // Eliminamos todo el HTML
+    });
 }
 
 // Funciones
@@ -18,8 +33,81 @@ function agregarCurso(e) {
     }
 }
 
-function leerDatosProducto(producto) {
-    console.log(producto);
+
+function eliminarCurso(e) {
+    if (e.target.classList.contains('borrar-producto')) {
+        const productoId = e.target.getAttribute('data-id');
+        articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
+        carritoHTML();
+    }
 }
 
+function leerDatosProducto(producto) {
+    console.log(producto);
 
+    // Crear un objeto con el contenido del producto actual
+    const infoProducto = {
+        imagen: producto.querySelector('img').src,
+        titulo: producto.querySelector('h2').textContent,
+        precio: producto.querySelector('.precio span').textContent,
+        id: producto.querySelector('button').getAttribute('data-id'),
+        cantidad: 1
+
+    };
+
+    // Revisa si un producto ya existe en el carrito
+    const existe = articulosCarrito.some(producto => producto.id === infoProducto.id);
+
+    if (existe) {
+        // Actualizamos la cantidad
+        const productos = articulosCarrito.map(producto => {
+            if (producto.id === infoProducto.id) {
+                producto.cantidad++;
+                return producto; // Retorna el objeto actualizado
+            } else {
+                return producto;
+            }
+        });
+        articulosCarrito = [...productos];
+    } else {
+        // Agregar el producto al carrito
+        articulosCarrito = [...articulosCarrito, infoProducto];
+    };
+
+    carritoHTML();
+};
+
+function carritoHTML() {
+    // Limpiar el HTML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach(producto => {
+        const { imagen, titulo, precio, cantidad, id } = producto;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td><img src="${imagen}" width="100"></td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-producto" data-id="${id}"> X </a>
+            </td>
+        `;
+
+        contenedorCarrito.appendChild(row);
+    });
+
+    // Agregar el carrito de compras al storage
+    sincronizarStorage();
+}
+
+function sincronizarStorage() {
+    localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
+}
+
+function limpiarHTML() {
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
